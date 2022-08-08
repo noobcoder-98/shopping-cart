ed805b0ea7b7d3a71fe702c4fa0bd18e1931f527
noobcoder-98 <anoobcoder98@gmail.com>
2022-08-08 17:48:57 +0700

    feat: create backend project, connect to database and build simple api

---
+++ API/Program.cs
@@ -0,0 +1,45 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefautlConnection");
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connect to database
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(connectionString));
+
+var app = builder.Build();
+
+// Initialize data
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+try
+{
+  context.Database.Migrate();
+  DbInitializer.Initialize(context);
+}
+catch (Exception e)
+{
+  logger.LogError(e, "Problem migrating data");
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
